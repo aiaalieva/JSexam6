11c48731a8586238e414984368f1a12b7851ac6c
Aiana Alieva <inalieva.a@gmail.com>
2018-12-01 18:23:50 +0600

    Task 6. Created function that prepends new messages to the newsfeed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(function () {
-    var container = $('.container');
     var send = $('.send');
     var post = $('#input-message');
     var newsfeed = $('.newsFeed');
@@ -9,6 +8,7 @@ $(function () {
     var editlastName = $('.lastName');
     var saveProfileChanges = $('.remodal-confirm');
     var emailField = $('.email');
+    var updatedDatetime;
 
 
     var getProfile = function () {
@@ -50,14 +50,14 @@ $(function () {
     };
 
     var showNewsfeed = function (response) {
-        console.log(response);
         for (var i = 0; i < response.length; i++) {
             var name = response[i].user.firstName;
             var lastName = response[i].user.lastName;
             var posts = response[i].message;
             var postBlock = $('<div class="postBlock">');
             postBlock.html(name + ' ' + lastName + ' said: <br>' + posts + '<br>' + response[i].datetime);
-            newsfeed.prepend(postBlock)
+            newsfeed.prepend(postBlock);
+            updatedDatetime = response[i].datetime;
         }
     };
 
@@ -75,40 +75,29 @@ $(function () {
 
     var subscribeRequest = function () {
         followBtn.on('click', function () {
-            if(emailField.val() === '') {
+            if (emailField.val() === '') {
                 alert('Fileds can\'t be empty')
             } else {
-                return $.post('http://146.185.154.90:8000/blog/inalieva.a@gmail.com/subscribe', {
+                return $.post('http://146.185.154.90:8000/blog/inalieva.a@gmail.com/subscribe/', {
                     email: emailField.val()
-                });
+                }).then(emailField.val(''));
             }
         })
     };
 
-    // var getNewPosts = function () {
-    //     var currentDatetime = new Date;
-    //     var updatedDatetime = currentDatetime.toISOString();
-    //     return $.ajax({
-    //         method: 'GET',
-    //         url: 'http://146.185.154.90:8000/blog/inalieva.a@gmail.com/posts?datetime=' + updatedDatetime
-    //     });
-    // };
+    var getNewPosts = function () {
+        return $.ajax({
+            method: 'GET',
+            url: 'http://146.185.154.90:8000/blog/inalieva.a@gmail.com/posts?datetime=' + updatedDatetime
+        })
+    };
 
-    // var showNewPosts = function (response) {
-    //     setInterval(function () {
-    //         console.log(response);
-    //         for (var i = 0; i < response.length; i++) {
-    //             if(response[i].datetime > updatedDatetime) {
-    //                 var name = response[i].user.firstName;
-    //                 var lastName = response[i].user.lastName;
-    //                 var posts = response[i].message;
-    //                 var postBlock = $('<div class="postBlock">');
-    //                 postBlock.html(name + ' ' + lastName + ' said: <br>' + posts + '<br>' + response[i].datetime);
-    //                 newsfeed.prepend(postBlock)
-    //             }
-    //         }
-    //     }, 3000);
-    // };
+    var interval = function () {
+        setInterval(function () {
+            getNewPosts()
+                .then(showNewsfeed)
+        }, 3000)
+    };
 
     getProfile()
         .then(displayProfile)
@@ -117,34 +106,6 @@ $(function () {
         .then(showNewsfeed)
         .then(createPost)
         .then(subscribeRequest)
-        // .then(getNewPosts)
-        // .then(showNewPosts)
+        .then(interval)
 
-
-
-//
-//     var sendMessage = function(){
-//         return $.post('http://146.185.154.90:8000/messages', {
-//             author: author.val(),
-//             message: message.val()
-//         });
-//     };
-//
-//     var interval = function () {
-//         setInterval(function () {
-//             getCurrentChat()
-//                 .then(showChats)
-//         }, 2000);
-//     };
-//
-//     getCurrentChat()
-//         .then(showChats)
-//         .then(interval);
-//
-//
-//     send.on('click', function () {
-//         sendMessage();
-//
-//     });
-//
 });
